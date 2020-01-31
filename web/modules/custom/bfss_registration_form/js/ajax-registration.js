f0dada91dbed6b467867f75d9670bbe177e22865
Ryan Perry <rperry@mindimage.net>
2020-01-31 05:34:03 +0000

    good for now

--- web/modules/custom/bfss_registration_form/js/ajax-registration.js
+++ web/modules/custom/bfss_registration_form/js/ajax-registration.js
@@ -3,11 +3,39 @@
     attach: function (context, settings) {
       $('.step-1 .panel-title').click(function() {
         $('.step-2 .panel-body').removeClass('in');
+         changeTheSign("one");
       });
 
       $('.step-2 .panel-title').click(function() {
         $('.step-1 .panel-body').removeClass('in');
+        changeTheSign("two");
       });
+
+      changeTheSign();
+
+      function changeTheSign(step) {
+
+        /* Step 1 */
+          if((step === 'one') || (step === '')) {
+              if ($('.step-1 .panel-title').attr('aria-expanded') === 'true') {
+                  $('.step-1 .panel-title img').attr('src', '/sites/default/files/images/forms/bfss-register-form-icon-minus.png');
+                  $('.step-2 .panel-title img').attr('src','/sites/default/files/images/forms/bfss-register-form-icon-plus.png')
+              } else {
+                  $('.step-1 .panel-title img').attr('src', '/sites/default/files/images/forms/bfss-register-form-icon-plus.png');
+                  $('.step-2 .panel-title img').attr('src','/sites/default/files/images/forms/bfss-register-form-icon-minus.png')
+              }
+          }
+          /* Step 2 */
+          if((step === 'two') || (step === '')) {
+              if ($('.step-2 .panel-title').attr('aria-expanded') === 'true') {
+                  $('.step-2 .panel-title img').attr('src', '/sites/default/files/images/forms/bfss-register-form-icon-plus.png');
+                  $('.step-1 .panel-title img').attr('src','/sites/default/files/images/forms/bfss-register-form-icon-plus.png')
+              } else {
+                  $('.step-2 .panel-title img').attr('src', '/sites/default/files/images/forms/bfss-register-form-icon-minus.png');
+                  $('.step-1 .panel-title img').attr('src','/sites/default/files/images/forms/bfss-register-form-icon-plus.png')
+              }
+          }
+      }
     }
   };
 })(jQuery, Drupal);
