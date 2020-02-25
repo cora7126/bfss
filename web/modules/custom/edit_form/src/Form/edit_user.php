1406d025796e27ecb45f6bde04bb819b3231bcf6
Ryan Perry <rperry@mindimage.net>
2020-02-25 07:47:21 +0000

    blah

--- web/modules/custom/edit_form/src/Form/edit_user.php
+++ web/modules/custom/edit_form/src/Form/edit_user.php
@@ -53,7 +53,7 @@ class edit_user extends FormBase {
       '#placeholder' => t('Jodi'),
        //'#default_values' => array(array('id')),
       '#default_value' => $results1['field_first_name_value'],
-	  '#prefix'=>'<div class="left_section"><div class="athlete_left"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>Athletic Information</h3><div class=items_div>',
+	  '#prefix'=>'<div class="left_section"><div class="athlete_left"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>Login Information</h3><div class=items_div>',
       );
     $form['lname'] = array(
       '#type' => 'textfield',
@@ -106,107 +106,7 @@ class edit_user extends FormBase {
       '#placeholder' => t('2020'),
       '#default_value' => '',
       );
-    $form['height'] = array (
-      '#type' => 'textfield',
-      //'#title' => ('Height'),
-      '#placeholder' => t('Height in Inches'),
-      '#default_value' => '',
-      );
-     $form['weight'] = array (
-      '#type' => 'textfield',
-      //'#title' => ('Height'),
-      '#placeholder' => t('Weight in Pounds'),
-      '#default_value' => '',
-	  '#suffix' => '</div></div>',
-      );
-	  $form['aboutme'] = array (
-      '#type' => 'textarea',
-      '#default_value' => '',
-	  '#prefix' => '<div class = "athlete_left"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>About me</h3><div class=items_div>',
-	  '#suffix' => '</div></div>',
-      );
-	  $form['instagram'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('Your Instagram Account(Optional)'),
-	  '#prefix' => '<div class = "athlete_left"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>Social Media<i class="fa fa-info right-icon" aria-hidden="true"></i></h3><div class=items_div>',
-      '#default_value' => '',
-      );
-     $form['youtube'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('Your Youtube/Video Channel(Optional)'),
-	  '#suffix' => '</div></div>',
-      '#default_value' => '',
-      );
-	  $form['education'] = array(
-		//'#title' => t('az'),
-		'#type' => 'select',
-		//'#description' => 'Select the desired pizza crust size.',
-		'#options' => array(t('--- Highschol ---'), t('10"'), t('12"'), t('16"')),
-	  '#prefix' => '<div class="athlete_school"><div class = "athlete_left"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>School/Club/University</h3><i class="fa fa-trash right-icon delete_icon" aria-hidden="true"></i><div class=items_div>',
-      );
-	
-	  $form['schoolname'] = array(
-		//'#title' => t('az'),
-		'#type' => 'select',
-		//'#description' => 'Select the desired pizza crust size.',
-		'#options' => array(t('--- Williams Highschol ---'), t('10"'), t('12"'), t('16"')),
-      );
-		$form['coach'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t("Coache's Last Name (Optional)"),
-      '#default_value' => '',
-      );
-	  $form['sport'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('Sport'),
-      '#default_value' => '',
-      );
-	  $form['position'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('Position'),
-      '#default_value' => '',
-	  '#prefix' => '<div class="add_pos_div">',
-	  '#suffix' => '<a class="add_pos"><i class="fa fa-plus"></i>Add Position</a><a class="remove_pos"><i class="fa fa-trash"></i>Remove Position</a></div>',
-      );
-	  $form['stats'] = array (
-      '#type' => 'textarea',
-	  '#suffix' => '</div></div><a class="add_org"><i class="fa fa-plus"></i>Add Another Organization</a></div></div><div class ="right_section"><div class = "athlete_right"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>My Website Photo</h3><div class="edit_dropdown"><a class="drop" >Action<span class="down-arrow fa fa-angle-down"></span></a><ul class="dropdown-menu" style="padding:0"></ul></div><div class=items_div>',
-      '#default_value' => '',
-      );
-     // $form['club'] = array(
-		// '#type' => 'select',
-		// '#options' => array(t('--- club ---'), t('10"'), t('12"'), t('16"')),
-	  // '#prefix' => '<div class = "athlete_part_five">',
-      // );
-	  // $form['clubname'] = array(
-		// '#type' => 'select',
-		// '#options' => array(t('--- Select Here,If Organization not listed ---'), t('10"'), t('12"'), t('16"')),
-      // );
-		// $form['clubnamenew'] = array (
-      // '#type' => 'textfield',
-      // '#placeholder' => t("Club name new"),
-      // '#default_value' => '',
-      // );
-	  // $form['coach_club'] = array (
-      // '#type' => 'textfield',
-      // '#placeholder' => t("Coache's Last Name (Optional)"),
-      // '#default_value' => '',
-      // );
-	  // $form['sport_club'] = array (
-      // '#type' => 'textfield',
-      // '#placeholder' => t('Sport'),
-      // '#default_value' => '',
-      // );
-	  // $form['position_club'] = array (
-      // '#type' => 'textfield',
-      // '#placeholder' => t('Position'),
-      // '#default_value' => '',
-      // );
-	  // $form['stats_club'] = array (
-      // '#type' => 'textarea',
-	  // '#suffix' => '</div>',
-      // '#default_value' => '',
-      // );
+    
 	  $form['image_athlete'] = [
                             '#type' => 'managed_file',
                             '#upload_validators' => [
@@ -220,85 +120,9 @@ class edit_user extends FormBase {
 							'#prefix' => '</div>',
 							'#suffix' =>'<div class="action_bttn"><span>Action</span><ul><li>Remove</li></ul></div></div></div>',
                           ];				  
-	   $form['school_web'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('school'),
-      '#default_value' => '',
-	  '#prefix' => '</div></div><div class = "athlete_right"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>My Website</h3><div class=items_div>',
-      );
-	   $form['sport_web'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('sport'),
-      '#default_value' => '',
-      );
-	   $form['name_web'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('Pick a Name'),
-      '#default_value' => '',
-	  '#prefix' => '<div class="container-inline">',
-'#suffix' => '</div>',
 
-	  '#attributes' => array('id'=>'name_1'),
-      );
-	  $form['label_1'] = array (
-      '#type' => 'label',
-      '#title' => '.bfsscience.com',
-	  '#attributes' => array('id'=>'label_1'),
-      );
-	  $form['label_2'] = array (
-      '#type' => 'label',
-      '#title' => 'Create your unique website profile.<br> eg.jodibloggs.bfsscience.com.<br> Once published , this will become your permanent address and it can not be changed.<br>',
-      );
-	  $form['preview_1'] = array (
-      '#type' => 'button',
-      '#default_value' => 'Preview Changes',
-      );
-	  $form['web_visible_1'] = array(
-		//'#title' => t('az'),
-		'#type' => 'select',
-		//'#description' => 'Select the desired pizza crust size.',
-		'#options' => array(t('--- Select Here,If Organization not listed ---'), t('10"'), t('12"'), t('16"')),
-		'#suffix' => '</div>',
-      );
-   $form['school_web2'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('school'),
-      '#default_value' => '',
-	  '#prefix' => '</div><div class = "athlete_right"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div>Additional Website</h3><div class=items_div>',
-      );
-	   $form['sport_web2'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('sport'),
-      '#default_value' => '',
-      );
-	   $form['name_web2'] = array (
-      '#type' => 'textfield',
-      '#placeholder' => t('Pick a Name'),
-      '#default_value' => '',
-	  '#prefix' => '<div class="container-inline">',
-'#suffix' => '</div>',
-	  '#attributes' => array('id'=>'name_2'),
-      );
-	  $form['label_12'] = array (
-      '#type' => 'label',
-      '#title' => '.bfsscience.com',
-	  '#attributes' => array('id'=>'label_2'),
-      );
-	  $form['label_22'] = array (
-      '#type' => 'label',
-      '#title' => 'Create your unique website profile.<br> eg.jodibloggs.bfsscience.com.<br> Once published , this will become your permanent address and it can not be changed.<br>',
-      );
-	  $form['preview_12'] = array (
-      '#type' => 'button',
-      '#default_value' => 'Preview Changes',
-      );
-	  $form['web_visible_2'] = array(
-		//'#title' => t('az'),
-		'#type' => 'select',
-		//'#description' => 'Select the desired pizza crust size.',
-		'#options' => array(t('--- Select Here,If Organization not listed ---'), t('10"'), t('12"'), t('16"')),
-		'#suffix' => '</div></div></div></div>',
-      );
+	  
+	
    
     $form['submit'] = [
         '#type' => 'submit',
