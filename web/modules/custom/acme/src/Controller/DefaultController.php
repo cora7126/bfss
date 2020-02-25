84bfd441b8c8c9523c8c5a39eac885ba7a909efb
Ryan Perry <rperry@mindimage.net>
2020-02-25 07:39:50 +0000

    Yesterdays Work

--- web/modules/custom/acme/src/Controller/DefaultController.php
+++ web/modules/custom/acme/src/Controller/DefaultController.php
@@ -9,11 +9,17 @@ class DefaultController extends ControllerBase {
   public function dashboard() {
     // the {name} in the route gets captured as $name variable
     // in the function called
-	
+	 #ATTACH BLOCK
+    $block = \Drupal\block\Entity\Block::load('assessmentscustomview');
+    $block_content = \Drupal::entityManager()
+      ->getViewBuilder('block')
+      ->view($block);
+    $assessments_block = \Drupal::service('renderer')->renderRoot($block_content);
     return [
       '#cache' => ['max-age' => 0,],
       '#theme' => 'hello_page',
       '#name' => 'Shubham Rana',
+      '#assessments_block' => $assessments_block,
       '#attached' => [
         'library' => [
           'acme/acme-styles', //include our custom library for this response
