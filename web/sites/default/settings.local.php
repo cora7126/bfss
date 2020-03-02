4942c2ec0ab0b696a186d433fadbee6cd0100f66
Ryan Perry <rperry@mindimage.net>
2020-03-02 05:08:01 +0000

    Yesterdays Work

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -153,3 +153,13 @@ $settings['skip_permissions_hardening'] = TRUE;
  * the language or field module.
  */
 # $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
+$databases['default']['default'] = array (
+  'database' => 'bfss_temp',
+  'username' => 'root',
+  'password' => 'GibTri05X*IV',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
