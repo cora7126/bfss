3a38998b60ccfad58b8d139751c13ab060b45a93
Ryan Perry <rperry@mindimage.net>
2020-03-13 07:01:17 +0000

    Yesterdays Work

---
+++ web/modules/custom/bfss_assessment/src/Controller/BfssManager.php
@@ -0,0 +1,41 @@
+<?php
+namespace Drupal\bfss_assessment\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class BfssManager extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function Bfss_Manager() {
+    
+    // $block = \Drupal\block\Entity\Block::load('upcominggroupassessments');
+    // $block_content = \Drupal::entityManager()
+    //   ->getViewBuilder('block')
+    //   ->view($block);
+    // $assessments_block = \Drupal::service('renderer')->renderRoot($block_content);
+
+    // return [
+    //   '#cache' => ['max-age' => 0,],
+    //   '#theme' => 'upcoming_page',
+    //   '#assessments_block' => $assessments_block,
+    //   '#attached' => [
+    //     'library' => [
+    //       'acme/acme-styles', //include our custom library for this response
+    //     ]
+    //   ]
+    // ];
+    $element = array(
+      '#markup' => 'upcoming group assessments',
+    );
+    return $element;
+  }
+
+}
