c0b2bcf5b50ebdd503de4fbda6f46a3dd55f7140
Ryan Perry <rperry@mindimage.net>
2020-04-16 20:09:33 +0000

    Yesterdays Work

--- web/themes/custom/bfss_custom/js/custom-jquery.js
+++ web/themes/custom/bfss_custom/js/custom-jquery.js
@@ -7,4 +7,18 @@ jQuery('.dashboard-menu span').on('click',function(){
 	 console.log("hr1");
 });
  
+
+	i = 0;
+	jQuery('#edit-organizations-plx').on('click',function(){
+		var arr = [];
+		jQuery('.edit-ckeckbox-plx:checked').each(function () {
+			arr[i++] = $(this).val();
+		});
+		console.log(jQuery.isEmptyObject(arr));
+		//if(jQuery.isEmptyObject(arr) == false){
+			var arrStr = encodeURIComponent(JSON.stringify(arr));
+			window.location.href = "http://5ppsystem.com/edit-organizations?nids="+arrStr;
+		//}
+
+	});
 });
