988a767f2c56ea4d2a549c47331c790f00b10b07
Ryan Perry <rperry@mindimage.net>
2020-06-18 03:23:18 +0000

    Yesterdays Work

--- web/modules/custom/bfss_registration_form/js/ajax-popup.js
+++ web/modules/custom/bfss_registration_form/js/ajax-popup.js
@@ -2,16 +2,31 @@
 (function($, Drupal) {
   Drupal.behaviors.bfss_registration_form = {
     attach: function (context, settings) {
-
+       // console.log(drupalSettings.payment.status.payment_status);
     	jQuery("#paymentfailModal span.closepopup.close").on("click", function(){
 			location.href = '/user/register';
-		});  
+		});
+
+        jQuery("#coachModal span.closepopup.close").on("click", function(){
+            location.href = '/user/login';
+        }); 
+
+        jQuery("#ForgetPassModal span.closepopup.close").on("click", function(){
+            location.href = '/user/login';
+        }); 
+        
     	if(drupalSettings.payment.status.payment_status == true){
     		loadModal('#requestCallbackModal');
     	}
     	if(drupalSettings.payment.status.payment_status == false){
     		loadModal('#paymentfailModal');
     	}	
+        if(drupalSettings.payment.status.payment_status == 'NOTNEED'){
+            loadModal('#coachModal');
+        }   
+         if(drupalSettings.payment.status.payment_status == 'FORGETPASSWORD'){
+            loadModal('#ForgetPassModal');
+        } 
     }
   }
 })(jQuery, Drupal);
