cd6d0be5ff99690f1754a89bbba8dc66fdd5071d
Jody Brabec <jodybrabec@gmail.com>
2020-07-06 21:13:09 -0700

    ticket

--- web/modules/custom/bfss_ticket_support/src/Form/CreateTicket.php
+++ web/modules/custom/bfss_ticket_support/src/Form/CreateTicket.php
@@ -23,138 +23,199 @@ class CreateTicket extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-  $current_user = \Drupal::currentUser()->id();
-//    $conn = Database::getConnection();
-//    $query2 = \Drupal::database()->select('user__field_state', 'ufs');
-//    $query2->addField('ufs', 'field_state_value');
-//    $query2->condition('entity_id', $current_user,'=');
-//    $results2 = $query2->execute()->fetchAssoc();
-//    $state = $results2['field_state_value'];
+    $current_user = \Drupal::currentUser()->id();
+    //    $conn = Database::getConnection();
+    //    $query2 = \Drupal::database()->select('user__field_state', 'ufs');
+    //    $query2->addField('ufs', 'field_state_value');
+    //    $query2->condition('entity_id', $current_user,'=');
+    //    $results2 = $query2->execute()->fetchAssoc();
+    //    $state = $results2['field_state_value'];
+
    $form['header'] = array(
-      '#prefix' => '<div class="main_header 1111111111111111"><h1 style="margin-top: 10px;font-size:15px;margin-left: 20px;"><i class="fas fa-home" style="color: #f76907;margin-right: 5px;"></i><i class="fas fa-angle-right" style="font-weight:400;margin-right:5px;"></i><a href="/dashboard" class="edit_dash" style="margin-right:5px;font-weight: bold;">Dashboard</a><i class="fas fa-angle-right" style="font-weight:400;margin-right:5px;"></i><a class="edit_dash" style="font-weight: bold;">Support</a><i class="fas fa-angle-right" style="font-weight:400;margin-right:5px;"></i><a class="edit_dash" style="font-weight: bold;">Ticketing</a></h1><div class="edit_header" style="display:flex; padding:15px;background: #fffcd7;border: 1px solid grey;"><i class="fa fa-laptop edit_image" aria-hidden="true"></i><h2 style="margin-top:0px;margin-bottom:0px;"><span style="font-size:13px;font-weight:600;">SUBMIT A</span><br>Ticket</h2></div>',
+      '#prefix' => '<div class="main_header">
+      <h1 style="margin-top: 10px;font-size:15px;margin-left: 20px;">
+        <i class="fas fa-home" style="color: #f76907;margin-right: 5px;"></i>
+        <i class="fas fa-angle-right" style="font-weight:400;margin-right:5px;"></i>
+        <a href="/dashboard" class="edit_dash" style="margin-right:5px;font-weight: bold;">Dashboard</a>
+        <i class="fas fa-angle-right" style="font-weight:400;margin-right:5px;"></i>
+        <a class="edit_dash" style="font-weight: bold;">Support</a>
+        <i class="fas fa-angle-right" style="font-weight:400;margin-right:5px;"></i>
+        <a class="edit_dash" style="font-weight: bold;">Ticketing</a></h1>
+        <div class="edit_header" style="display:flex; padding:15px;background: #fffcd7;border: 1px solid grey;">
+        <i class="fa fa-laptop edit_image" aria-hidden="true"></i>
+        <h2 style="margin-top:0px;margin-bottom:0px;">
+          <span style="font-size:13px;font-weight:600;">
+          SUBMIT A TICKET</span><br>
+        </h2>
+        <div><br>
+          &nbsp; We will respond to your ticket at our earliest availability<br>
+        </div>
+        </div>',
       '#suffix' => '</div>',
     );
-  $form[sub_header] = array( 
-    '#prefix' => '<div class="left_section popup_left_section"><div class="athlete_left"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class= "        fa fa-plus hide"></i></div>
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-         <a class="nav-link active" data-toggle="tab" href="#home">Open Ticket</a>
-        </li>
-        <li class="nav-item">
-         <a class="nav-link" data-toggle="tab" href="#menu1">Resolved Ticket</a>
-        </li>
-    </ul>
-    </h3><div class=items_div>',
-     '#suffix' => '</div>',
-      );
-   
-   
+
     $form['subject'] = array(
       '#type' => 'textfield',
       '#required' => TRUE,
       '#placeholder' => t('Subject'),
-      '#prefix' => '<div class = "athlete_right"><h3><div class="toggle_icon"><i class="fa fa-minus"></i><i class="fa fa-plus hide"></i></div><i class="fa fa-info right-icon" aria-hidden="true"></i></h3><div class=items_div>',
-//      '#default_value' => $results3['field_first_name_value'],
-      );
-    $form['comment'] = array(
+      // '#default_value' => $results3['field_first_name_value'],
+    );
+    $form['description'] = array(
       '#type' => 'textarea',
       '#placeholder' => t('Questions/Comments'),
-//      '#default_value' => $results1['field_last_name_value'],
-      );
+      // '#default_value' => $results1['field_last_name_value'],
+    );
+    $form['create_ticket'] = array(
+      '#type' => 'hidden',
+      '#value' => '1',
+    );
     $form['submit'] = [
-        '#type' => 'submit',
-        '#value' => 'save',
-		'#prefix' =>'<div id="create_tct_submit">',
-		'#suffix' => '</div></div>',
-        //'#value' => t('Submit'),
+      '#type' => 'submit',
+      '#value' => 'save',
+  		'#prefix' =>'<div id="create_tct_submit">',
+      //'#value' => t('Submit'),
     ];
-   
+
     return $form;
   }
-  
+
   public function validateForm(array &$form, FormStateInterface $form_state) {
-         // $name = $form_state->getValue('jodi');
-          // if(preg_match('/[^A-Za-z]/', $name)) {
-             // $form_state->setErrorByName('jodi', $this->t('your jodi must in characters without space'));
-          // }
-        // if (!is_float($form_state->getValue('height'))) {
-             // $form_state->setErrorByName('candidate_age', $this->t('Height needs to be a number'));
-            // }
-        // if (!is_float($form_state->getValue('weight'))) {
-             // $form_state->setErrorByName('candidate_age', $this->t('Weight needs to be a number'));
-            // }
-         /* $number = $form_state->getValue('candidate_age');
-          if(!preg_match('/[^A-Za-z]/', $number)) {
-             $form_state->setErrorByName('candidate_age', $this->t('your age must in numbers'));
-          }*/
-//          if (strlen($form_state->getValue('mobile_number')) < 10 ) {
-//            $form_state->setErrorByName('mobile_number', $this->t('your mobile number must in 10 digits'));
-//           }
-    // parent::validateForm($form, $form_state);
+      // $name = $form_state->getValue('jodi');
+      // if(preg_match('/[^A-Za-z]/', $name)) {
+          // $form_state->setErrorByName('jodi', $this->t('your jodi must in characters without space'));
+      // }
+    // if (!is_float($form_state->getValue('height'))) {
+          // $form_state->setErrorByName('candidate_age', $this->t('Height needs to be a number'));
+        // }
+    // if (!is_float($form_state->getValue('weight'))) {
+          // $form_state->setErrorByName('candidate_age', $this->t('Weight needs to be a number'));
+        // }
+      /* $number = $form_state->getValue('candidate_age');
+      if(!preg_match('/[^A-Za-z]/', $number)) {
+          $form_state->setErrorByName('candidate_age', $this->t('your age must in numbers'));
+      }*/
+      //     if (strlen($form_state->getValue('mobile_number')) < 10 ) {
+      //       $form_state->setErrorByName('mobile_number', $this->t('your mobile number must in 10 digits'));
+      //      }
+      // parent::validateForm($form, $form_state);
   }
+
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-//      $popupFlag = 'filled';
-//    $field=$form_state->getValues();
-//    $jodi=$field['fname'];
-//    //echo "$name";
-//    $bloggs=$field['lname'];
-//    $az=$field['state'];
-//    $city=$field['city'];
-//    $gender=$field['birth_gender'];
-//    $dob=$field['field_dob'];
-//    $height = $field['height'];
-//    $weight = $field['weight'];
-//    $organizaton_type=$field['organization_type'];
-//    $organizaton_name=$field['organization_name'];
-//    $coach_lname=$field['coach_lname'];
-//    $sport=$field['sport'];
-//    $position=$field['position'];
-//    $instagram=$field['instagram'];
-//    $youtube=$field['youtube'];
-//      
-//           $field  = array(
-//              'field_jodi'   => $jodi,
-//              'field_bloggs' =>  $bloggs,
-//              'field_az' =>  $az,
-//              'field_city' => $city,
-//              'field_birth_gender' => $gender,
-//              'field_dob' => $dob,
-//              'field_height' => $height,
-//              'field_weight' => $weight,
-//              'field_organization_type' => $organizaton_type,
-//              'field_organization_name' => $organizaton_name,
-//              'field_coach_lname' => $coach_lname,
-//              'field_sport' => $sport,
-//              'field_position' => $position,
-//              'field_instagram' => $instagram,
-//              'field_youtube' => $youtube,
-//          );
-//		    $current_user = \Drupal::currentUser()->id();
-//    $conn = Database::getConnection();
-//	$conn->insert('mydata')->fields(
-//	array(
-//              'field_jodi'   => $jodi,
-//              'field_bloggs' =>  $bloggs,
-//              'field_az' =>  $az,
-//              'field_city' => $city,
-//              'field_birth_gender' => $gender,
-//              'field_dob' => $dob,
-//              'field_height' => $height,
-//              'field_weight' => $weight,
-//              'field_organization_type' => $organizaton_type,
-//              'field_organization_name' => $organizaton_name,
-//              'field_coach_lname' => $coach_lname,
-//              'field_sport' => $sport,
-//              'field_position' => $position,
-//              'field_instagram' => $instagram,
-//              'field_youtube' => $youtube,
-//              'popup_flag' => $popupFlag,
-//              'uid' => $current_user,
-//          )
-//	)->execute();
-//       $form_state->setRedirect('acme_hello');
-     }
+
+    if (@$_POST['create_ticket']) {
+      /**
+       * Sends form data (ticket_data) to freshdesk.
+       * customer sso security
+       */
+      $api_key = "6aTnr07ieoIsXLhN1c0";
+      $password = "99999"; // not needed, keep as x
+      $yourdomain = "digitalrace";
+
+      $ticket_data = json_encode(array(
+        "description" => $_POST['description'],
+        "subject" => $_POST['subject'],
+        "email" => 'ashnsugar@gmail.com',
+        "priority" => 1,
+        "status" => 2,
+        "cc_emails" => array("ashnsugar@gmail.com")
+      ));
+
+      $url = "https://$yourdomain.freshdesk.com/api/v2/tickets";
+
+      $ch = curl_init($url);
+
+      $header[] = "Content-type: application/json";
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+      curl_setopt($ch, CURLOPT_HEADER, true);
+      curl_setopt($ch, CURLOPT_USERPWD, "$api_key:$password");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $ticket_data);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+      $server_output = curl_exec($ch);
+      $info = curl_getinfo($ch);
+      $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+      $headers = substr($server_output, 0, $header_size);
+      $response = substr($server_output, $header_size);
+
+      if($info['http_code'] == 201) {
+        // ksm("Ticket created successfully, the response is given below \n");
+        // ksm("$headers\n");
+        // ksm("$response \n");
+      } else {
+        if($info['http_code'] == 404) {
+          ksm("Error, Please check the end point \n");
+        } else {
+          ksm("Error, HTTP Status Code : " . $info['http_code'] . "\n");
+          ksm("Headers are ".$headers);
+          ksm("Response are ".$response);
+        }
+      }
+      curl_close($ch);
+    }
+
+    //    $popupFlag = 'filled';
+    //    $field=$form_state->getValues();
+    //    $jodi=$field['fname'];
+    //    //echo "$name";
+    //    $bloggs=$field['lname'];
+    //    $az=$field['state'];
+    //    $city=$field['city'];
+    //    $gender=$field['birth_gender'];
+    //    $dob=$field['field_dob'];
+    //    $height = $field['height'];
+    //    $weight = $field['weight'];
+    //    $organizaton_type=$field['organization_type'];
+    //    $organizaton_name=$field['organization_name'];
+    //    $coach_lname=$field['coach_lname'];
+    //    $sport=$field['sport'];
+    //    $position=$field['position'];
+    //    $instagram=$field['instagram'];
+    //    $youtube=$field['youtube'];
+    //
+    //           $field  = array(
+    //              'field_jodi'   => $jodi,
+    //              'field_bloggs' =>  $bloggs,
+    //              'field_az' =>  $az,
+    //              'field_city' => $city,
+    //              'field_birth_gender' => $gender,
+    //              'field_dob' => $dob,
+    //              'field_height' => $height,
+    //              'field_weight' => $weight,
+    //              'field_organization_type' => $organizaton_type,
+    //              'field_organization_name' => $organizaton_name,
+    //              'field_coach_lname' => $coach_lname,
+    //              'field_sport' => $sport,
+    //              'field_position' => $position,
+    //              'field_instagram' => $instagram,
+    //              'field_youtube' => $youtube,
+    //          );
+    //		    $current_user = \Drupal::currentUser()->id();
+    //    $conn = Database::getConnection();
+    //	$conn->insert('mydata')->fields(
+    //	array(
+    //              'field_jodi'   => $jodi,
+    //              'field_bloggs' =>  $bloggs,
+    //              'field_az' =>  $az,
+    //              'field_city' => $city,
+    //              'field_birth_gender' => $gender,
+    //              'field_dob' => $dob,
+    //              'field_height' => $height,
+    //              'field_weight' => $weight,
+    //              'field_organization_type' => $organizaton_type,
+    //              'field_organization_name' => $organizaton_name,
+    //              'field_coach_lname' => $coach_lname,
+    //              'field_sport' => $sport,
+    //              'field_position' => $position,
+    //              'field_instagram' => $instagram,
+    //              'field_youtube' => $youtube,
+    //              'popup_flag' => $popupFlag,
+    //              'uid' => $current_user,
+    //          )
+    //	)->execute();
+    //       $form_state->setRedirect('acme_hello');
+  }
 }
---
+++ web/modules/custom/bfss_ticket_support/src/Form/fresh-create.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<body>
+
+
+<?php
+
+$user_email = 'TO-BE-SET@xxxxxx.com';
+echo "<div style='padding:22px;'>Current logged on email (id): $user_email</div>";
+
+if (@$_POST['create_ticket']) {
+	/**
+	 * Sends form data (ticket_data) to freshdesk.
+	 * customer sso security
+	 */
+	$api_key = "6aTnr07ieoIsXLhN1c0";
+	$password = "x"; // not needed, keep as x
+	$yourdomain = "digitalrace";
+
+	$ticket_data = json_encode(array(
+	"description" => $_POST['description'],
+	"subject" => $_POST['subject'],
+	"email" => $user_email,
+	"priority" => 1,
+	"status" => 2,
+	"cc_emails" => array("jodybrabec@gmail.com")
+	));
+
+	$url = "https://$yourdomain.freshdesk.com/api/v2/tickets";
+
+	$ch = curl_init($url);
+
+	$header[] = "Content-type: application/json";
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_USERPWD, "$api_key:$password");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $ticket_data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$server_output = curl_exec($ch);
+	$info = curl_getinfo($ch);
+	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	$headers = substr($server_output, 0, $header_size);
+	$response = substr($server_output, $header_size);
+
+	if($info['http_code'] == 201) {
+		echo "Ticket created successfully, the response is given below \n";
+		// echo "Response Headers are \n";
+		// echo $headers."\n";
+		// echo "Response Body \n";
+		echo "$response \n";
+	} else {
+	if($info['http_code'] == 404) {
+		echo "Error, Please check the end point \n";
+	} else {
+		echo "Error, HTTP Status Code : " . $info['http_code'] . "\n";
+		echo "Headers are ".$headers;
+		echo "Response are ".$response;
+	}
+	}
+
+	curl_close($ch);
+}
+
+?>
+
+<div style="font-weight:bold; font-size: 1.3em;">New ticket</div>
+
+We will respond to your ticket at our earliest availability
+<form method="POST">
+	<input type="hidden" name="create_ticket" value="true" />
+	<input type="text" name="subject" value="" placeholder="Subject"  style="width: 50%;" /><br><br>
+	Problem Description:<br>
+	<textarea name="description" style="width: 50%; height: 300px;"></textarea><br>
+	<input type="submit" />
+</form>
+
+
+</body>
+</html>
